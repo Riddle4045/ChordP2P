27169a8362ea1f781a2fad21b568abc02e276bff
Ishan Patwa <Ishan@Ishans-MacBook-Pro.local>
2015-10-24 12:23:54 -0400

    finished skeleton code

--- Project3.scala
+++ Project3.scala
@@ -1,20 +1,33 @@
-import akka.actor.ActorSystem;
-import akka.actor.ActorSelection;
-import akka.actor.Actor;
-import akka.actor.ActorRef;
-import akka.actor.Props;
-import akka.actor.Actor;
-import akka.actor.ActorIdentity;
-import akka.actor.Identify;
-import akka.actor.Props;
-import akka.pattern.AskableActorSelection;
-import akka.util.Timeout;
-import java.math.BigInteger;
-import akka.actor.ActorRef;
-import com.sun.org.apache.xpath.internal.operations.Bool;
-import scala.collection.immutable.HashMap;
+import akka.actor.ActorSystem
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.actor.Props
+import com.sun.org.apache.xpath.internal.operations.Bool
+import scala.collection.immutable.HashMap
+import java.lang.Long
+import scala.concurrent.duration.Duration
+import scala.actors.threadpool.TimeUnit
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.actor.ActorSelection
+import java.util.Timer
+import scala.util.Success
+import scala.util.Failure
+import java.math.BigInteger
+import scala.util.control.Breaks
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.Await
 
 
+trait Message
+case object getSucessor extends Message
+case class Find_Sucessor(n : Int) extends Message
+case class Find_Predecessor(n : Int)extends Message
+case class Set_Predecessor(n : Int) extends Message
+case class Update_Finger_Table(n : Int,i : Int) extends Message
 
 
 class Master extends Actor {
@@ -31,7 +44,7 @@ class Master extends Actor {
   }
 }
 
-class Node(numNodes : Int , m:Int, hashOfFistNode : String) extends Actor {
+class Node(numNodes : Int , m:Int, hashOfFistNode : Int, system : ActorSystem) extends Actor {
           //TODO : implement a data strcuture to hold finger table for the guy.
           //TODO : impleemnt three functions find_sucessor find_predecessor and closest_preceding_finger.
   
@@ -43,24 +56,32 @@ class Node(numNodes : Int , m:Int, hashOfFistNode : String) extends Actor {
   
   // private values of a node 
   var fingerTable = 0;
-  var sucessor = null;
-  var  predecessor = null;
-  var nodeId 
+  var sucessor = 0;
+  var  predecessor = 0;
+  var nodeId =0;
   
   def receive = {
     case s : String => println("got a string");
     case "firstNode"  => initFingerTable();
-    case "New Node" => join(hashOfFirstNode);
+    case "New Node" => join(hashOfFistNode);
+    case getSucessor => sender ! sucessor
+    case Find_Sucessor(id) => sender ! find_successor(id)
+    case Find_Predecessor(id) => sender! find_predecessor(id)
+    case Set_Predecessor(id) => predecessor = id;
+    case Update_Finger_Table(n,i) => update_finger_table(n, i)
   }
   
+  def initFingerTable() ={
+    
+  }
 
   def join(n : Int ) = {
-       if(isNode(n)){
+       if(isExsist(n)){
                init_finger_table(n);
-               ipdate_tables(n);
+               update_others();
        }else{
              for( i <- 1 to m){
-                   finger[i][0] = nodeId;
+                   finger(i)(0)= nodeId;
              }
              predecessor = nodeId;
        }
@@ -68,70 +89,138 @@ class Node(numNodes : Int , m:Int, hashOfFistNode : String) extends Actor {
   }
   
   def init_finger_table(n : Int) = {
-      
+       var n_actor = getActorRef(n);
+       implicit val timeout = Timeout(1 seconds)
+       var n_future =  n_actor ? Find_Sucessor(finger(1)(0));
+       finger(1)(1) = Await.result(n_future,timeout.duration).asInstanceOf[Int]
+       sucessor = finger(1)(1);
+       var m_actor = getActorRef(finger(1)(1));
+       var m_future = m_actor ? Find_Predecessor(finger(1)(1));
+       predecessor = Await.result(m_future,timeout.duration).asInstanceOf[Int]
+       m_actor ! Set_Predecessor(nodeId);
+       
+       
+       //now update the finger table.
+       for(i <- 1 to m-1){
+         if((finger(i+1)(0) >= n)&&(finger(i+1)(0) <  finger(i)(1))){
+           finger(i+1)(1) = finger(i)(1);
+         }
+         else{
+                  var o_actor = getActorRef(n);
+                  var o_future = o_actor ? Find_Sucessor(finger(i+1)(0));
+                  finger(i+1)(1) =  Await.result(o_future,timeout.duration).asInstanceOf[Int]
+         }
+       }
   }
   
   def update_others() = {
+
+      for(i<- 1 to m){
+        val index= nodeId - Math.pow(2,i-1).toInt;
+       var n_actor = getActorRef(index);
+       implicit val timeout = Timeout(1 seconds)
+       var n_future =  n_actor ? Find_Predecessor(finger(1)(0));
+       var p = Await.result(n_future,timeout.duration).asInstanceOf[Int]    
+       var p_actor = getActorRef(p);
+       p_actor ! Update_Finger_Table(nodeId,i);
+        
+      }
+  }
+  
+  def update_finger_table(s : Int , i : Int) = {
+      if(s>= nodeId && s<finger(i)(1)){
+        finger(i)(1) = s;
+         var p = predecessor;
+               var p_actor = getActorRef(p);
+       p_actor ! Update_Finger_Table(s,i);
+          
+      }
       
   }
+ 
   def find_successor(id:Int) : Int = {
     var temp = find_predecessor(id);
-   
-    return temp.successor;
+    var  temp_actor : ActorRef = null;
+    var temp_successor : Int = 0;
+   if (isExsist(temp)) {
+           temp_actor = getActorRef(temp);
+           implicit val timeout = Timeout(1 seconds)
+           var future = temp_actor ? getSucessor // enabled by the “ask” import
+           temp_successor = Await.result(future, timeout.duration).asInstanceOf[Int];
+           
+   }
+    return temp_successor;
    
   }
   
-  
-  
-  //this function will be called on init if a node.
-  //so in the beiging all the nodes will be sucessor and predecessor of themseleves.
-  //sets the value of the sucessor and predecessor private variable of the node.
- def sucessor(NodeId: Int) : Int = {
-   return 0
- 
- }
- 
+
  def find_predecessor(id : Int ) : Int = {
        var temp = nodeId;
-       var temp_successor = temp.successor;
-      while((id < temp) || (id>temp.successor)){
-         temp = closest_preceeding_finger(id);
+        var  temp_actor : ActorRef = null;
+        var temp_successor : Int = 0;
+       if (isExsist(temp)) {
+               temp_actor = getActorRef(temp);
+               implicit val timeout = Timeout(5 seconds)
+               var future = temp_actor ? getSucessor // enabled by the “ask” import
+               temp_successor = Await.result(future, timeout.duration).asInstanceOf[Int];
+               
+       }
+      while((id < temp) || (id > temp_successor)){
+         temp = closest_preceeding_finger(id,m);
        }
       return temp;
  }
  
- def closest_preceeding_finger(id : Int) : Int = {
-   for(i<-m to 1){
-     if((finger[i][1]>nodeId) && (finger[i][1] < id)){
-       return finger[i][1];
-     }
-     return nodeId;
-   }
-   
+ 
+ def closest_preceeding_finger(id : Int, m : Int) : Int = {
+         var res = nodeId;
+         var loop = new Breaks;
+         loop.breakable  {
+          for(  i <- m to 1 by -1)   {
+                        if( (finger(i)(1) > id) && (finger(i)(1) < nodeId) ){
+                                  res = finger(i)(1); loop.break;
+                        }
+          }
+         } 
+   return res;
  }
  
  //this function sends numRequests requests and upon completion stops sending requests
  //note that it doesnt shut down.
  def initaite_peer()={
-       
+       //initialize the table 
  }
  
- def checkExistence(id : Int) = {
-
-    ActorSystem sys = ActorSystem.apply("test");
-    sys.actorOf(Props.create(TestActor.class), "mytest");
-    ActorSelection sel = sys.actorSelection("/user/mytest");
-
-    Timeout t = new Timeout(5, TimeUnit.SECONDS);
-    AskableActorSelection asker = new AskableActorSelection(sel);
-    Future<Object> fut = asker.ask(new Identify(1), t);
-    ActorIdentity ident = (ActorIdentity)Await.result(fut, t.duration());
-    ActorRef ref = ident.getRef();
-    System.out.println(ref == null);
-   
+ //get the reference of the actor from the id
+ def getActorRef(temp : Int) : ActorRef ={
+             var isFound : ActorRef= null;
+                      implicit val timeout = 1000;
+                       var name = "akka://Master/user/" + temp.toString();
+                    system.actorSelection(name).resolveOne()(timeout).onComplete {
+                      case Success(actor) => 
+                                 isFound = actor;
+                      case Failure(ex) =>
+                              println("actor not found");
+                           
+                        }
+   return isFound;
+ }
+ 
+ //check if the actor with this id exsists.
+ def isExsist(id : Int) : Boolean = {
+    var isFound = false;
+                      implicit val timeout = 1000;
+                       var name = "akka://Master/user/" + id.toString();
+                    system.actorSelection(name).resolveOne()(timeout).onComplete {
+                      case Success(actor) => 
+                                 isFound = true;
+                      case Failure(ex) =>
+                              println("actor not found");
+                              isFound = false;
+                        }
+   return isFound;
 }
  
-
 
 }
 /**
@@ -142,13 +231,7 @@ object Project3 {
  
   def main(args:Array[String]){
     
-    //TODO : Extract the arguments as numNodes and numRequests
-    //TODO : figure out value of "m" from the numNodes.
-    //TODO : Initialize chord network through JOIN or iteratively.
-    //TODO : implement find+predecessor and successor functions .
-    //TODO : send messages from each peer.
-    //TODO : stop the program when all the peers send numRequests messages
-//    
+
     if(args.length < 2){
               System.out.println("Not enough arguments");
               
@@ -157,12 +240,7 @@ object Project3 {
       val numRequests = Integer.parseInt(args(1));
     
       
-   var hashOfFirstNode;
-   // save the id of first node we create.
-   //second node onwards join.
-    //m can be choosen such that 2^m -1 is nearly 1000 times the number of nodes?
-    //00000000111010101010101010101010101010..........
-    //m- bits -->   000000 to 1111111   -->> decimal number 0 to dec(1111111) 
+   var hashOfFirstNode = 0;
     var m=0;   //this is the m-bits from paper.
     var temp = 0;
     while ( temp < (numNodes*10000).toInt){
@@ -180,12 +258,6 @@ object Project3 {
        actor = system.actorOf(Props(new Master), name = name);
      }
     
-     
-     //System.out.println(10.toBinaryString);
-    /* val binaryString = String.format("%"+Integer.toString(m)+"s",Integer.toBinaryString(m));
-     System.out.println(binaryString);*/
-    
-     
   }
    
  def createIdentifier(m:Int,nodeName:String) : String = {
